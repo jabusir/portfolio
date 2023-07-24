110023f4f57cf0ec092feafc0242cf0a5022afd8
Jehad Abusir <jabusir@Jehads-MacBook-Pro.local>
2023-07-23 21:57:45 -0700

    Restore padding

--- src/components/MobileLockScreen/index.js
+++ src/components/MobileLockScreen/index.js
@@ -62,7 +62,7 @@ export default function MobileLockScreen({ unlock }) {
 
   return (
     <div className="overflow-hidden">
-      <div className="flex items-center flex-col justify-between h-[90vh] overflow-hidden">
+      <div className="flex items-center flex-col justify-between h-[90vh] overflow-hidden py-10">
         <div className="flex flex-col items-center">
           <div>{localDate}</div>
           <div className="text-7xl font-bold">{localTime}</div>
